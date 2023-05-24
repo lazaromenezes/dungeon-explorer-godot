602389b027909e5d70f71f363426bbcef1d0597f
Lázaro Menezes <lazarodm@gmail.com>
2023-05-23 23:58:24 -0300

    Start adding enemies

---
+++ scenes/characters/enemies/enemy_character.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var enemy_stats: EnemyStats
+
+func define_health(health_dict: Dictionary):
+	if enemy_stats in health_dict.keys():
+		enemy_stats.set_current_health(health_dict[enemy_stats])
+		$HealthBar.call("set_progress_bar", enemy_stats.current_health)
