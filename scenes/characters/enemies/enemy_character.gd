b991021a7fb86f281c38998bf32d8fb05a94a67f
Lázaro Menezes <lazarodm@gmail.com>
2023-05-24 17:02:21 -0300

    Add enemy spawn mechanics

--- scenes/characters/enemies/enemy_character.gd
+++ scenes/characters/enemies/enemy_character.gd
@@ -6,3 +6,5 @@ func define_health(health_dict: Dictionary):
 	if enemy_stats in health_dict.keys():
 		enemy_stats.set_current_health(health_dict[enemy_stats])
 		$HealthBar.call("set_progress_bar", enemy_stats.current_health)
+		
+		visible = enemy_stats.current_health > 0
--- scenes/dungeon/dungeon.gd
+++ scenes/dungeon/dungeon.gd
@@ -1,6 +1,35 @@
 extends Node3D
 
-var hirelings: Array[HirelingStats]
+const MAX_HAZARDS: int = 7
+const DUNGEON_LEVEL_LABEL: String = "Nível da Dungeon: %s"
+
+@export var dungeon_level = 0
 
 func _ready():
 	get_tree().call_group("hirelings_group", "define_health", Party.current_party)
+	next_wave()
+	
+func next_wave():
+	dungeon_level += 1
+	_roll_hazards()
+	_update_hud()
+	
+func _roll_hazards():
+	var max_allowed_hazards = min(dungeon_level, MAX_HAZARDS);
+
+	var rolled = [EnemyStats]
+
+	for i in range(max_allowed_hazards):
+		rolled.append($Enemies.get_children().pick_random().enemy_stats);
+
+	var rolled_group: Dictionary = {}
+	
+	for enemy in rolled:
+		var count = rolled.count(enemy)
+		if count > 0:
+			rolled_group[enemy] = count
+			
+	get_tree().call_group("enemies_group", "define_health", rolled_group)
+	
+func _update_hud():
+	$HUD/DungeonLevelText.text = DUNGEON_LEVEL_LABEL % dungeon_level
